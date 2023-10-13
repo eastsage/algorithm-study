45f9b916ab9d8ddbfdff78754a325683884ad384
이동현 <80168287+eastsage@users.noreply.github.com>
2023-10-13 13:42:23 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] arr, result;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        visited = new boolean[n];
+        result = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        recur(0, 0);
+        System.out.println(sb);
+    }
+    static void recur(int depth, int start) {
+        if (depth >= m) {
+            for (int i : result) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            result[depth] = arr[i];
+            recur(depth + 1, i + 1);
+        }
+
+    }
+}
