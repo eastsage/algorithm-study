f6884956f0f8ca8713ddca63c6d9ea18f5746c95
이동현 <80168287+eastsage@users.noreply.github.com>
2023-10-13 13:26:23 +0900

    [Silver III] Title: N과 M (4), Time: 156 ms, Memory: 16424 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        recur(0, 1);
+        System.out.println(sb);
+    }
+    static void recur(int depth, int start) {
+        if (depth >= m) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            arr[depth] = i;
+            recur(depth + 1, i);
+        }
+
+    }
+}
