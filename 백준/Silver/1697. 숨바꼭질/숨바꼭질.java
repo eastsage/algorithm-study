bce194e706890ca15a8215c7c40a7d3a77cc750a
이동현 <80168287+eastsage@users.noreply.github.com>
2023-11-20 16:39:29 +0900

    [Silver I] Title: 숨바꼭질, Time: 260 ms, Memory: 106832 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,62 +1,45 @@
-import static java.lang.System.in;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, k;
+
+    static int n, a, b, k;
     static boolean[] visited;
+    static int[] map, footprint, tmp;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
         visited = new boolean[100_001];
-        if (n == k) {
-            System.out.println(0);
-            return;
-        }
-        bfs(0);
+        bfs();
     }
 
-    static void bfs(int cnt) {
-        Deque<Integer> q = new ArrayDeque<>();
-        q.offer(n);
+    static void bfs() {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{n, 0});
+        visited[n] = true;
 
         while (!q.isEmpty()) {
-            int size = q.size();
-            cnt++;
-            for (int i = 0; i < size; i++) {
-                int tmp = q.poll();
-                if (tmp + 1 <= 100000 && !visited[tmp + 1]) {
-                    if (tmp + 1 == k) {
-                        System.out.println(cnt);
-                        return;
-                    }
-                    q.offer(tmp + 1);
-                    visited[tmp + 1] = true;
-                }
-                if (tmp - 1 >= 0 && !visited[tmp - 1]) {
-                    if (tmp - 1 == k) {
-                        System.out.println(cnt);
-                        return;
-                    }
-                    q.offer(tmp - 1);
-                    visited[tmp - 1] = true;
-                }
-                if (tmp * 2 <= 100000 && !visited[tmp * 2]) {
-                    if (tmp * 2 == k) {
-                        System.out.println(cnt);
-                        return;
-                    }
-                    q.offer(tmp * 2);
-                    visited[tmp * 2] = true;
+            int[] cur = q.poll();
+            visited[cur[0]] = true;
+
+            if (cur[0] == k) {
+                System.out.println(cur[1]);
+                return;
+            }
+
+            int[] nxt = {cur[0] * 2, cur[0] + 1, cur[0] - 1};
+            for (int i = 0; i < 3; i++) {
+                if (nxt[i] >= 0 && nxt[i] <= 100_000 && !visited[nxt[i]]) {
+                    q.offer(new int[]{nxt[i], cur[1] + 1});
                 }
             }
         }
