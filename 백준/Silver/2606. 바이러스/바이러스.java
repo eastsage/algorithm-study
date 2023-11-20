b0e518dcd0e427c58fd395b77c39cfbac36f4451
이동현 <80168287+eastsage@users.noreply.github.com>
2023-11-20 16:45:39 +0900

    [Silver III] Title: 바이러스, Time: 132 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,44 +1,49 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    static int node[][];
-    static int check[];
 
-    static void bfs(int start) { // BFS 메소드
-        Queue<Integer> queue = new LinkedList<>();
+    static int m, n;
+    static long result;
+    static boolean[] visited;
+    static List<Integer>[] edges;
+    static int[] arr;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
 
-        check[start] = 1;
-        queue.offer(start);
-        int cnt = 0; // 감염 된 컴퓨터의 수
-        while (!queue.isEmpty()) {
-            int x = queue.poll();
-
-            for (int i = 1; i < node.length; i++) {
-                if (node[x][i] == 1 && check[i] != 1) {
-                    queue.offer(i);
-                    check[i] = 1;
-                    cnt++;
-                }
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        visited = new boolean[n + 1];
+        edges = new List[n + 1];
+        for (int i = 1; i <= n; i++) {
+            edges[i] = new ArrayList<>();
         }
-        System.out.println(cnt); //모든 탐색을 마치고 cnt 출력
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
+            edges[n1].add(n2);
+            edges[n2].add(n1);
+        }
+
+        dfs(1);
+
+        System.out.println(result - 1);
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // 컴퓨터의 수
-        int m = sc.nextInt(); // 네트워크 상에 연결되어 있는 컴퓨터 쌍의 수 즉, 간선의 수
-        node = new int[n + 1][n + 1];
-        check = new int[n + 1];
-        for (int i = 0; i < m; i++) { // 그래프 구성
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            node[a][b] = 1;
-            node[b][a] = 1;
+    private static void dfs(int node) {
+        visited[node] = true;
+        result++;
+        for (Integer nxt : edges[node]) {
+            if (visited[nxt]) continue;
+            dfs(nxt);
         }
-        bfs(1);
+
     }
 }
