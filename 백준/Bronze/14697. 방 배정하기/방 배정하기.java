c915732078606b44bb51344bdffabbdb8de1bc1a
이동현 <80168287+eastsage@users.noreply.github.com>
2024-01-01 23:22:54 +0900

    [Bronze II] Title: 방 배정하기, Time: 136 ms, Memory: 14300 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int result = 0;
+
+        for (int i = 0; i <= 50; i++)
+            for (int j = 0; j <= 50; j++)
+                for (int k = 0; k <= 50; k++)
+                    if ((a * i) + (b * j) + (c * k) == n)
+                        result = 1;
+
+        System.out.println(result);
+
+    }
+}
