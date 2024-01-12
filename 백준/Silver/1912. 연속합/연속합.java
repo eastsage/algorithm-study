e00e1f657a54a7f6148dbfa163e2c87955d5d98a
이동현 <80168287+eastsage@users.noreply.github.com>
2024-01-12 16:07:35 +0900

    [Silver II] Title: 연속합, Time: 264 ms, Memory: 23576 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int[] arr;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        int max = Integer.MIN_VALUE;
+        int[] prefixSum = new int[arr.length];
+        prefixSum[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            prefixSum[i] = prefixSum[i - 1] + arr[i];
+            if (prefixSum[i] < arr[i]) {
+                prefixSum[i] = arr[i];
+            }
+            max = Math.max(max, prefixSum[i]);
+        }
+//        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(prefixSum));
+//        System.out.println("max = " + max);
+        System.out.println(max);
+    }
+}
