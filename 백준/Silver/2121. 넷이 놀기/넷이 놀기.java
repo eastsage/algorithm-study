6d83488f74042ffbfa571d1adc6ebd3689609676
이동현 <80168287+eastsage@users.noreply.github.com>
2024-01-26 10:51:44 +0900

    [Silver III] Title: 넷이 놀기, Time: 1212 ms, Memory: 152720 KB -BaekjoonHub

--- 백준/Silver/2121. 넷이 놀기/넷이 놀기.java
+++ 백준/Silver/2121. 넷이 놀기/넷이 놀기.java
@@ -1,10 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -21,43 +21,50 @@ public class Main {
 		int a = Integer.parseInt(st.nextToken()); //가로
 		int b = Integer.parseInt(st.nextToken()); //세로
 
-		Set<Pair> pairs = new HashSet<>();
+		List<Pair> pairs = new ArrayList<>();
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			pairs.add(new Pair(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 		}
 
+		Collections.sort(pairs, (o1, o2) -> {
+			if (o1.x == o2.x)
+				return o1.y - o2.y;
+			return o1.x - o2.x;
+		});
+
 		int cnt = 0;
 		for (Pair pair : pairs) {
 			int x = pair.x;
 			int y = pair.y;
-			if (pairs.contains(new Pair(x + a, y)) &&
-				pairs.contains(new Pair(x, y + b)) &&
-				pairs.contains(new Pair(x + a, y + b))) {
+			if (binarySearch(pairs, x + a, y) &&
+				binarySearch(pairs, x, y + b) &&
+				binarySearch(pairs, x + a, y + b)) {
 				cnt++;
 			}
 		}
 		System.out.println(cnt);
 	}
 
-	// static boolean binarySearch(int low, int high, int key, char type) {
-	// 	while (low <= high) {
-	// 		int mid = (low + high) >>> 1;
-	// 		int midVal = arr[mid][0];
-	// 		if (type == 'y') {
-	// 			midVal = arr[mid][1];
-	// 		}
-	//
-	// 		if (midVal < key)
-	// 			low = mid + 1;
-	// 		else if (midVal > key)
-	// 			high = mid - 1;
-	// 		else {
-	// 			return true;
-	// 		}
-	// 	}
-	// 	return false;
-	// }
+	static boolean binarySearch(List<Pair> pairs, int x, int y) {
+		int low = 0;
+		int high = pairs.size() - 1;
+
+		while (low <= high) {
+			int mid = (low + high) >>> 1;
+			Pair pair = pairs.get(mid);
+
+			if (pair.x == x && pair.y == y) {
+				return true;
+			} else if (pair.x < x || (pair.x == x && pair.y < y)) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+		return false;
+	}
+
 	private static class Pair {
 		int x;
 		int y;
