e23bcc3c2f872972a11a1f22f912de577289daae
이동현 <80168287+eastsage@users.noreply.github.com>
2024-01-31 18:06:18 +0900

    [Silver III] Title: N과 M (5), Time: 348 ms, Memory: 28812 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -2,47 +2,51 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    static int[] arr, result;
-    static boolean[] visited;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	static int n, m, a, b, c;
+	static int[] arr, selected;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
 
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        arr = new int[n];
-        visited = new boolean[n];
-        result = new int[m];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(arr);
-        recur(0);
-        System.out.println(sb);
-    }
-    static void recur(int depth) {
-        if (depth >= m) {
-            for (int i : result) {
-                sb.append(i).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-        for (int i = 0; i < n; i++) {
-            if (visited[i]) continue;
-            visited[i] = true;
-            result[depth] = arr[i];
-            recur(depth + 1);
-            visited[i] = false;
-        }
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+		visited = new boolean[n];
+		selected = new int[m];
 
-    }
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		recur(0);
+		System.out.println(sb);
+	}
+
+	static void recur(int depth) {
+		if (depth == m) {
+			for (int i : selected) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (visited[i]) continue;
+			visited[i] = true;
+			selected[depth] = arr[i];
+			recur(depth + 1);
+			visited[i] = false;
+		}
+	}
 }
