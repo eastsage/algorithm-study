f8a489f68c91a2ae5c111089c13f486fe1a67d37
이동현 <80168287+eastsage@users.noreply.github.com>
2024-02-16 16:38:10 +0900

    [Gold V] Title: 평범한 배낭, Time: 240 ms, Memory: 54160 KB -BaekjoonHub

---
+++ 백준/Gold V/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, k, result;
+    static int[] w, v;
+    static int[][] dp;
+
+    /**
+     * 완탐 돌려서 가장 먼저 승리한 경우의 수의 승리자가 winner 고정이라고 할 수 있음
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        w = new int[n];
+        v = new int[n];
+        dp = new int[n][k + 100];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            w[i] = Integer.parseInt(st.nextToken());
+            v[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(recur(0, 0));
+    }
+    static int recur(int cur, int weight) {
+        if (weight > k) {
+            return -1123123123;
+        }
+        if (cur >= n) {
+            return 0;
+        }
+        if (dp[cur][weight] != -1) {
+            return dp[cur][weight];
+        }
+
+
+        dp[cur][weight] = Math.max(recur(cur + 1, weight), recur(cur + 1, weight + w[cur]) + v[cur]);
+        return dp[cur][weight];
+    }
+}
