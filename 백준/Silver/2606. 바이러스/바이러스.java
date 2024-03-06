c41b45082556422719e0a2ff6560173c57fb079a
이동현 <80168287+eastsage@users.noreply.github.com>
2024-03-06 18:30:12 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -3,47 +3,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
-
-    static int m, n;
-    static long result;
+class Main {
+    static int n, m, result;
+    static Set<Integer>[] edges;
     static boolean[] visited;
-    static List<Integer>[] edges;
-    static int[] arr;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         n = Integer.parseInt(br.readLine());
         m = Integer.parseInt(br.readLine());
-        visited = new boolean[n + 1];
-        edges = new List[n + 1];
-        for (int i = 1; i <= n; i++) {
-            edges[i] = new ArrayList<>();
-        }
 
-        for (int i = 0; i < m; i++) {
+        visited = new boolean[n + 1];
+        edges = new Set[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            edges[i] = new HashSet();
+        }
+        for (int i = 1; i <= m; i++) {
             st = new StringTokenizer(br.readLine());
             int n1 = Integer.parseInt(st.nextToken());
             int n2 = Integer.parseInt(st.nextToken());
             edges[n1].add(n2);
             edges[n2].add(n1);
         }
-
         dfs(1);
-
         System.out.println(result - 1);
     }
 
-    private static void dfs(int node) {
-        visited[node] = true;
+    static void dfs(int cur) {
         result++;
-        for (Integer nxt : edges[node]) {
-            if (visited[nxt]) continue;
-            dfs(nxt);
-        }
+        visited[cur] = true;
 
+        for (Integer next : edges[cur]) {
+            if (visited[next]) continue;
+            dfs(next);
+        }
     }
 }
