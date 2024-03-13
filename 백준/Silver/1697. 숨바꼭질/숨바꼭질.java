27904dfa49b8fa94479cb211420d5a471cccf10a
이동현 <80168287+eastsage@users.noreply.github.com>
2024-03-13 09:55:19 +0900

    [Silver I] Title: 숨바꼭질, Time: 168 ms, Memory: 17756 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,47 +1,58 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
 
-    static int n, a, b, k;
+class Main {
+    static int n, k, result;
+    static int[] m;
     static boolean[] visited;
-    static int[] map, footprint, tmp;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
+    static int[] dx = {-1, 1, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-        visited = new boolean[100_001];
+        visited = new boolean[210_000];
+
+        if (n == k) {
+            System.out.println(0);
+            return;
+        }
+
         bfs();
+        System.out.println(result);
     }
 
     static void bfs() {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{n, 0});
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(n);
         visited[n] = true;
 
         while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            visited[cur[0]] = true;
+            int size = q.size();
+            result++;
 
-            if (cur[0] == k) {
-                System.out.println(cur[1]);
-                return;
-            }
+            for (int i = 0; i < size; i++) {
+                Integer cur = q.poll();
 
-            int[] nxt = {cur[0] * 2, cur[0] + 1, cur[0] - 1};
-            for (int i = 0; i < 3; i++) {
-                if (nxt[i] >= 0 && nxt[i] <= 100_000 && !visited[nxt[i]]) {
-                    q.offer(new int[]{nxt[i], cur[1] + 1});
+                for (int j = 0; j < 3; j++) {
+                    int nx = cur + dx[j];
+                    if (nx == cur) {
+                        nx = cur * 2;
+                    }
+
+                    if (nx < 0 || nx > 100_000) continue;
+                    if (visited[nx]) continue;
+                    if (nx == k) return;
+                    q.offer(nx);
+                    visited[nx] = true;
                 }
             }
         }
+
     }
 }
