ed246874f4db7980b97d1751b165325f544fe475
이동현 <80168287+eastsage@users.noreply.github.com>
2024-07-02 15:35:11 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 536 ms, Memory: 51620 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,89 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
-    static int V, E, result, cnt;
-    static int[] parents;
-    static Edge[] edges;
+
+class Main {
+    static int n, m, x, result;
+    static boolean[] visited;
+    static int[] parent;
+    static List<int[]> edges;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-        parents = new int[V + 1];
-        edges = new Edge[E];
-        init();
-
-        for (int i = 0; i < E; i++) {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        edges = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            edges[i] = new Edge(from, to, cost);
+            edges.add(new int[]{v1, v2, cost});
         }
-        Arrays.sort(edges);
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+        Collections.sort(edges, Comparator.comparingInt(a -> a[2]));
 
-        for (Edge edge : edges) {
-            if (union(edge.to, edge.from)) {
-                result += edge.cost;
-                if (++cnt == V - 1) {
-                    System.out.println(result);
-                    return;
-                }
+        result = 0;
+        for (int[] edge : edges) {
+            if (find(edge[0]) != find(edge[1])) {
+                result += edge[2];
+                union(edge[0], edge[1]);
             }
         }
+        System.out.println(result);
 
     }
 
-    static boolean union(int a, int b) {
+    static int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    static void union(int a, int b) {
         a = find(a);
         b = find(b);
-
-        if (a != b) {
-            if (a <= b) {
-                parents[a] = b;
-            } else {
-                parents[b] = a;
-            }
-            return true;
-        }
-        return false;
+        parent[b] = a;
     }
 
-    static int find(int a) {
-        if (parents[a] == a) {
-            return a;
-        } else {
-            return parents[a] = find(parents[a]);
-        }
-    }
 
-    static void init() {
-        for (int i = 0; i < V; i++) {
-            parents[i] = i;
-        }
-    }
-
-    private static class Edge implements Comparable<Edge> {
-        int from;
-        int to;
-        int cost;
-
-        public Edge(int from, int to, int cost) {
-            this.from = from;
-            this.to = to;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Edge o) {
-            return this.cost - o.cost;
-        }
-    }
 }
