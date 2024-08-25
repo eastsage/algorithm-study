e9d2bc257927ca46100147388bd60022a5f1eedd
이동현 <80168287+eastsage@users.noreply.github.com>
2024-08-25 19:14:00 +0900

    [Gold V] Title: 빗물, Time: 112 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -1,48 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] map;
-    static int sum;
+	static int h, w;
+	static int[] map;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		h = Integer.parseInt(st.nextToken());
+		w = Integer.parseInt(st.nextToken());
+		map = new int[w];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < w; i++) {
+			map[i] = Integer.parseInt(st.nextToken());
+		}
+		long sum = 0;
+		for (int i = 1; i < map.length - 1; i++) {
+			int lPeek = 0;
+			int rPeek = 0;
 
-    public static void main(String[] args) throws IOException {
-        input();
+			for (int j = i - 1; j >= 0; j--) {
+				lPeek = Math.max(lPeek, map[j]);
+			}
 
-        // 현재 idx에서 왼쪽에서 peek 오른쪽에서 peek를 찾고
-        // 그 중에서 작은놈 - 본인 == 물의 양
-        // 양 끝은 안됨
+			for (int j = i + 1; j < map.length; j++) {
+				rPeek = Math.max(rPeek, map[j]);
+			}
 
-        for (int i = 1; i < map.length - 1; i++) {
-            int lPeek = 0;
-            int rPeek = 0;
-
-            for (int j = i - 1; j >= 0; j--) {
-                lPeek = Math.max(lPeek, map[j]);
-            }
-
-            for (int j = i + 1; j < map.length; j++) {
-                rPeek = Math.max(rPeek, map[j]);
-            }
-
-            if (map[i] < lPeek && map[i] < rPeek) {
-                sum += Math.min(lPeek, rPeek) - map[i]; // 더 낮은 봉우리 만큼 물이 고임
-            }
-        }
-        System.out.println(sum);
-    }
-
-    private static void input() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        st.nextToken(); // height 필요 없음
-        int width = Integer.parseInt(st.nextToken());
-        map = new int[width];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < map.length; i++) {
-            map[i] = Integer.parseInt(st.nextToken());
-        }
-    }
+			if (map[i] < lPeek && map[i] < rPeek) {
+				sum += Math.min(lPeek, rPeek) - map[i]; // 더 낮은 봉우리 만큼 물이 고임
+			}
+		}
+		System.out.println(sum);
+	}
 }
