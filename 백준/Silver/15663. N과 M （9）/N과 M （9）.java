137cbe5eb59182c02d1a4f157efb061810402a95
이동현 <80168287+eastsage@users.noreply.github.com>
2024-10-22 16:14:11 +0900

    [Silver II] Title: N과 M (9), Time: 436 ms, Memory: 53352 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,18 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static int n, m;
-    static int[] s, e;
-    static int[] arr, selected;
-    static char[][][] map;
+    static int[] selected, arr;
+    static int[][] map, dp;
     static boolean[] visited;
-    static int[] dx = {1, -1, 0, 0, 0, 0};
-    static int[] dy = {0, 0, 1, -1, 0, 0};
-    static int[] dz = {0, 0, 0, 0, 1, -1};
+    static Set<List<Integer>> set = new LinkedHashSet<>();
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
@@ -21,39 +17,36 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
-        arr = new int[n];
-        visited = new boolean[n];
+        visited = new boolean[n + 1];
         selected = new int[m];
-
+        arr = new int[n];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-
+        }
         Arrays.sort(arr);
-
-        dfs(0);
+        recur(0);
+        for (List<Integer> list : set) {
+            for (Integer i : list) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
     }
 
-    static void dfs(int cnt) {
-        if (cnt == m) {
-            for (int i = 0; i < m; i++)
-                System.out.print(selected[i] + " ");
-            System.out.println();
-        } else {
-            int before = 0;
-            for (int i = 0; i < n; i++) {
-                if (visited[i])
-                    continue;
-
-                if (before != arr[i]) {
-                    visited[i] = true;
-                    selected[cnt] = arr[i];
-                    before = arr[i];
-                    dfs(cnt + 1);
-                    visited[i] = false;
-                }
-            }
+    static void recur(int depth) {
+        if (depth == m) {
+            Integer[] array = Arrays.stream(selected).boxed().toArray(Integer[]::new);
+            set.add(Arrays.asList(array));
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            selected[depth] = arr[i];
+            recur(depth + 1);
+            visited[i] = false;
         }
     }
 }
