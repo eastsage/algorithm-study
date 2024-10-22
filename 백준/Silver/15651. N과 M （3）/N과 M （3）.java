041e9bb49901eefabe9a6a9de22304f94629b10b
이동현 <80168287+eastsage@users.noreply.github.com>
2024-10-22 15:40:01 +0900

    [Silver III] Title: N과 M (3), Time: 392 ms, Memory: 68524 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,37 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     static int n, m;
-    static int[] arr;
+    static int[] selected;
+    static int[][] map, dp;
     static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        arr = new int[m];
+        visited = new boolean[n + 1];
+        selected = new int[m];
         recur(0);
         System.out.println(sb);
     }
+
     static void recur(int depth) {
-        if (depth >= m) {
-            for (int i : arr) {
+        if (depth == m) {
+            for (int i : selected) {
                 sb.append(i).append(" ");
             }
             sb.append("\n");
             return;
         }
-
         for (int i = 1; i <= n; i++) {
-            arr[depth] = i;
+            selected[depth] = i;
             recur(depth + 1);
         }
-
     }
 }
