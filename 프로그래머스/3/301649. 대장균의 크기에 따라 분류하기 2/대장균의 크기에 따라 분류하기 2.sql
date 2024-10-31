e1d51fe3c1022242ecd7979ac4f5d40aa7164174
이동현 <80168287+eastsage@users.noreply.github.com>
2024-10-31 18:01:43 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -0,0 +1,15 @@
+
+select a.id, 
+case
+    when a.per <= 0.25 then 'CRITICAL'
+    when a.per <= 0.5 then 'HIGH'
+    when a.per <= 0.75 then 'MEDIUM'
+    else 'LOW'
+end as colony_name
+    
+from (
+    select id, 
+    percent_rank() over (order by size_of_colony desc) as per
+    from ecoli_data
+) a
+order by a.id
