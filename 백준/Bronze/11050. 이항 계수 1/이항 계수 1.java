ea828b6b93256b6d08fdf68a772badfeb0a0aa3b
이동현 <80168287+eastsage@users.noreply.github.com>
2024-11-08 17:50:40 +0900

    [Bronze I] Title: 이항 계수 1, Time: 108 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,28 +1,40 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
- 
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
- 
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
- 
-		// nCk = n! / ((n-k)! * k!)
-		System.out.println(factorial(N) / (factorial(N - K) * factorial(K)));
-	}
- 
-	static int factorial(int N) {
-		// factorial(0) == 1 이다. 
-		if (N <= 1)	{
-			return 1;
-		}
-		return N * factorial(N - 1);
-	}
+    static int n, m, k;
+    static int[] dx = {1, 0};
+    static int[] dy = {0, 1};
+    static int[][] arr;
+    static int[][] map;
+    static int[] dp;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        dp = new int[n + 100];
+        Arrays.fill(dp, -1);
+
+        System.out.println(factorial(n) / (factorial(n - k) * factorial(k)));
+    }
+
+    static int factorial(int cur) {
+        // factorial(0) == 1 이다. 
+        if (cur <= 1) {
+            return 1;
+        }
+        if (dp[cur] != -1) return dp[cur];
+
+
+        return dp[cur] = cur * factorial(cur - 1);
+    }
+
 }
