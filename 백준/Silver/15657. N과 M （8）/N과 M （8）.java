bc119675be434f47aa02d831109d25a613c0a5b2
이동현 <80168287+eastsage@users.noreply.github.com>
2025-01-10 13:54:14 +0900

    [Silver III] Title: N과 M (8), Time: 144 ms, Memory: 17620 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -1,38 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    static int[] selected, arr;
-    static int[][] map, dp;
+    static int[] arr, selected;
     static boolean[] visited;
+    static int n, m;
     static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        visited = new boolean[n + 1];
-        selected = new int[m];
-        arr = new int[n];
+
         st = new StringTokenizer(br.readLine());
+        arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
+
+        selected = new int[m];
+        visited = new boolean[n + 1];
         recur(0, 0);
         System.out.println(sb);
     }
 
-    static void recur(int depth, int start) {
-        if (depth == m) {
+    static void recur(int cur, int start) {
+        if (cur == m) {
             for (int i : selected) {
                 sb.append(i).append(" ");
             }
@@ -40,11 +38,8 @@ public class Main {
             return;
         }
         for (int i = start; i < n; i++) {
-//            if (visited[i]) continue;
-//            visited[i] = true;
-            selected[depth] = arr[i];
-            recur(depth + 1, i);
-//            visited[i] = false;
+            selected[cur] = arr[i];
+            recur(cur + 1, i);
         }
     }
 }
