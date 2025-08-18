e168b370a305bb81ac8bbbc04990e875f1edd331
이동현 <80168287+eastsage@users.noreply.github.com>
2025-08-18 14:31:17 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 35.69 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public Stack<Integer> solution(int []arr) {
+        int[] answer = {};
+        Stack<Integer> s = new Stack<>();
+        
+        s.push(arr[0]);
+        for(int i=1; i<arr.length; i++) {
+            if(s.peek() != arr[i]) {
+                s.push(arr[i]);
+            }
+        }
+
+        return s;
+    }
+}
