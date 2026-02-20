dbb64e74742438ea75bc4df2d0a93556dcd0c108
이동현 <80168287+eastsage@users.noreply.github.com>
2026-02-20 21:07:44 +0900

    [level 3] Title: N으로 표현, Time: 31.10 ms, Memory: 94.5 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,49 +1,61 @@
 import java.util.*;
+
 class Solution {
-    /**
-    1 ~ 32,000    
-    1
-    2 - 11
-    3 - 21 12
-    4 - 31 13 22
-    5 - 14 41 23 32
-    6 - 15 51 24 42 33
-    ...
-    **/
-    public int solution(int n, int number) {
-        Set[] sets = new Set[9];
-        for(int i=1; i<9; i++) {
-            sets[i] = new HashSet<Integer>();
+    private int N;
+    private int number;
+    private Map<Integer, Set<Integer>> memo = new HashMap<>();
+
+    public int solution(int iN, int iNumber) {
+        this.N = iN;
+        this.number = iNumber;
+
+        if (N == number) return 1;
+
+        // 1개부터 8개까지 탑다운으로 계산
+        for (int cnt = 1; cnt <= 8; cnt++) {
+            Set<Integer> candidates = recur(cnt);
+            if (candidates.contains(number)) {
+                return cnt;
+            }
         }
-        sets[1].add(n);
-        
-        for(int i=2; i<9; i++) {
-            for(int j=1; j<i; j++) {
-                Set<Integer> pre = sets[j];
-                Set<Integer> post = sets[i-j];
-                
-                for(Integer preNum : pre) {
-                    for(Integer postNum : post) {
-                        sets[i].add(preNum + postNum);
-                        sets[i].add(preNum - postNum);
-                        sets[i].add(preNum * postNum);
-                        if(postNum != 0 && preNum != 0) {
-                            sets[i].add(preNum / postNum);
-                        }
+        return -1;
+    }
+
+    // recur(cnt): N을 cnt번 사용해서 만들 수 있는 모든 값
+    private Set<Integer> recur(int cnt) {
+        if (memo.containsKey(cnt)) {
+            return memo.get(cnt);
+        }
+
+        Set<Integer> result = new HashSet<>();
+
+        // 1) 이어붙인 수 추가 (5, 55, 555 ...)
+        int repeated = 0;
+        for (int i = 0; i < cnt; i++) {
+            repeated = repeated * 10 + N;
+        }
+        result.add(repeated);
+
+        // 2) cnt를 (left + right)로 분할해서 조합
+        for (int left = 1; left < cnt; left++) {
+            int right = cnt - left;
+
+            Set<Integer> leftSet = recur(left);
+            Set<Integer> rightSet = recur(right);
+
+            for (int a : leftSet) {
+                for (int b : rightSet) {
+                    result.add(a + b);
+                    result.add(a - b);
+                    result.add(a * b);
+                    if (b != 0) {
+                        result.add(a / b); // 정수 나눗셈
                     }
                 }
             }
-            sets[i].add(Integer.parseInt(String.valueOf(n).repeat(i)));
         }
-        for(int i=1; i<9; i++) {
-            Set<Integer> set = sets[i];
-            for(Integer num : set) {
-                if(num == number) {
-                    return i;
-                }
-            }
-        }
-        
-        return -1;
+
+        memo.put(cnt, result);
+        return result;
     }
 }
